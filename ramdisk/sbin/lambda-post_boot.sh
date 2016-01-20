c1c32210449d2ad4af264820b3f5e491be698f08
Ícaro Hoff <icarohoff@gmail.com>
2016-01-19 23:06:42 -0200

    lambda-post_boot: Comment out not support for 3.4.108
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- ramdisk/sbin/lambda-post_boot.sh
+++ ramdisk/sbin/lambda-post_boot.sh
@@ -11,31 +11,31 @@ stop mpdecision
 ############################
 # MSM_Hotplug Settings
 #
-echo "[Lambda Kernel] Tunning MSM Hotplug values" | tee /dev/kmsg
-echo 1 > /sys/module/msm_hotplug/min_cpus_online
-echo 2 > /sys/module/msm_hotplug/cpus_boosted
-echo 500 > /sys/module/msm_hotplug/down_lock_duration
-echo 3000 > /sys/module/msm_hotplug/boost_lock_duration
-echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
-echo 100 > /sys/module/msm_hotplug/fast_lane_load
-echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
+#echo "[Lambda Kernel] Tunning MSM Hotplug values" | tee /dev/kmsg
+#echo 1 > /sys/module/msm_hotplug/min_cpus_online
+#echo 2 > /sys/module/msm_hotplug/cpus_boosted
+#echo 500 > /sys/module/msm_hotplug/down_lock_duration
+#echo 3000 > /sys/module/msm_hotplug/boost_lock_duration
+#echo 200 5:100 50:50 350:200 > /sys/module/msm_hotplug/update_rates
+#echo 100 > /sys/module/msm_hotplug/fast_lane_load
+#echo 1 > /sys/module/msm_hotplug/max_cpus_online_susp
 
 ############################
 # MSM_MPDecision (Bricked) Hotplug Settings
 #
-echo 652800 > /sys/kernel//msm_mpdecision/conf/idle_freq
+#echo 652800 > /sys/kernel//msm_mpdecision/conf/idle_freq
 
 ############################
 # CPU-Boost Settings
 #
-echo "[Lambda Kernel] Setting boost parameters" | tee /dev/kmsg
-echo 20 > /sys/module/cpu_boost/parameters/boost_ms
-echo 500 > /sys/module/cpu_boost/parameters/input_boost_ms
-echo 0:1497600 1:1497600 2:1497600 3:1497600 > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
-echo 25 > /sys/module/cpu_boost/parameters/migration_load_threshold
-echo 1 > /sys/module/cpu_boost/parameters/hotplug_boost
-echo 1 > /sys/module/cpu_boost/parameters/wakeup_boost
+#echo "[Lambda Kernel] Setting boost parameters" | tee /dev/kmsg
+#echo 20 > /sys/module/cpu_boost/parameters/boost_ms
+#echo 500 > /sys/module/cpu_boost/parameters/input_boost_ms
+#echo 0:1497600 1:1497600 2:1497600 3:1497600 > /sys/module/cpu_boost/parameters/input_boost_freq
+#echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
+#echo 25 > /sys/module/cpu_boost/parameters/migration_load_threshold
+#echo 1 > /sys/module/cpu_boost/parameters/hotplug_boost
+#echo 1 > /sys/module/cpu_boost/parameters/wakeup_boost
 
 ############################
 # Tweak Background Writeout
@@ -49,7 +49,7 @@ echo 50 > /proc/sys/vm/vfs_cache_pressure
 ############################
 # Power Effecient Workqueues
 #
-echo 1 > /sys/module/workqueue/parameters/power_efficient
+#echo 1 > /sys/module/workqueue/parameters/power_efficient
 
 ############################
 # MSM Limiter
@@ -63,20 +63,24 @@ echo 2265600 > /sys/kernel/msm_limiter/resume_max_freq_0
 echo 2265600 > /sys/kernel/msm_limiter/resume_max_freq_1
 echo 2265600 > /sys/kernel/msm_limiter/resume_max_freq_2
 echo 2265600 > /sys/kernel/msm_limiter/resume_max_freq_3
-echo 1190400 > /sys/kernel/msm_limiter/suspend_max_freq
+#echo 1190400 > /sys/kernel/msm_limiter/suspend_max_freq
 
 ############################
 # Scheduler and Read Ahead
 #
-echo fiops > /sys/block/mmcblk0/queue/scheduler
-echo 512 > /sys/block/mmcblk0/bdi/read_ahead_kb
+#echo fiops > /sys/block/mmcblk0/queue/scheduler
+echo 1024 > /sys/block/mmcblk0/bdi/read_ahead_kb
 
 ############################
 # Governor Tunings
 #
 
-# Non-intrusive interactive tweaks
+# Intrusive interactive tweaks
 echo "[Lambda Kernel] Tunning interactive governor" | tee /dev/kmsg
+echo interactive > /sys/kernel/msm_limiter/scaling_governor_0
+echo interactive > /sys/kernel/msm_limiter/scaling_governor_1
+echo interactive > /sys/kernel/msm_limiter/scaling_governor_2
+echo interactive > /sys/kernel/msm_limiter/scaling_governor_3
 echo 20000 1400000:40000 1700000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
 echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
 echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
@@ -88,34 +92,30 @@ echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
 echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_slack
 
 # Intrusive smartmax tweaks
-echo "[Lambda Kernel] Tunning smartmax governor" | tee /dev/kmsg
-echo smartmax > /sys/kernel/msm_limiter/scaling_governor_0
-echo smartmax > /sys/kernel/msm_limiter/scaling_governor_1
-echo smartmax > /sys/kernel/msm_limiter/scaling_governor_2
-echo smartmax > /sys/kernel/msm_limiter/scaling_governor_3
-echo 729600 > /sys/devices/system/cpu/cpufreq/smartmax/suspend_ideal_freq
-echo 1036800 > /sys/devices/system/cpu/cpufreq/smartmax/awake_ideal_freq
-echo 1 > /sys/devices/system/cpu/cpufreq/smartmax/io_is_busy
-echo 70 > /sys/devices/system/cpu/cpufreq/smartmax/max_cpu_load
-echo 30 > /sys/devices/system/cpu/cpufreq/smartmax/min_cpu_load
-echo 1497600 > /sys/devices/system/cpu/cpufreq/smartmax/touch_poke_freq
-echo 1497600 > /sys/devices/system/cpu/cpufreq/smartmax/boost_freq
+#echo "[Lambda Kernel] Tunning smartmax governor" | tee /dev/kmsg
+#echo 729600 > /sys/devices/system/cpu/cpufreq/smartmax/suspend_ideal_freq
+#echo 1036800 > /sys/devices/system/cpu/cpufreq/smartmax/awake_ideal_freq
+#echo 1 > /sys/devices/system/cpu/cpufreq/smartmax/io_is_busy
+#echo 70 > /sys/devices/system/cpu/cpufreq/smartmax/max_cpu_load
+#echo 30 > /sys/devices/system/cpu/cpufreq/smartmax/min_cpu_load
+#echo 1497600 > /sys/devices/system/cpu/cpufreq/smartmax/touch_poke_freq
+#echo 1497600 > /sys/devices/system/cpu/cpufreq/smartmax/boost_freq
 
 ############################
 # LMK Tweaks
 #
-echo "[Lambda Kernel] Setting LMK values" | tee /dev/kmsg
-echo 1536,2048,4096,16384,28672,32768 > /sys/module/lowmemorykiller/parameters/minfree
-echo 32 > /sys/module/lowmemorykiller/parameters/cost
-echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
+#echo "[Lambda Kernel] Setting LMK values" | tee /dev/kmsg
+#echo 1536,2048,4096,16384,28672,32768 > /sys/module/lowmemorykiller/parameters/minfree
+#echo 32 > /sys/module/lowmemorykiller/parameters/cost
+#echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
+#echo 53059 > /sys/module/lowmemorykiller/parameters/vmpressure_file_min
 
 ############################
 # Misc Tweaks
 #
-echo 0 > /sys/kernel/sched/gentle_fair_sleepers
-echo 1 > /sys/module/adreno_idler/parameters/adreno_idler_active
-echo 1 > /dev/cpuctl/cpu.notify_on_migrate
+#echo 0 > /sys/kernel/sched/gentle_fair_sleepers
+#echo 1 > /sys/module/adreno_idler/parameters/adreno_idler_active
+#echo 1 > /dev/cpuctl/cpu.notify_on_migrate
 
 ############################
 # Disable Debugging
