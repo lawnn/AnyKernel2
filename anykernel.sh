597722b92ba146ee8c68928450c1b9c39958bb9f
lawn <bloom.all.over.12@gmail.com>
2016-02-13 15:20:55 +0900

    AnyKernel2: cleaning old files

--- anykernel.sh
+++ anykernel.sh
@@ -8,9 +8,9 @@ do.devicecheck=1
 do.initd=1
 do.modules=0
 do.cleanup=1
-device.name1=maguro
-device.name2=toro
-device.name3=toroplus
+device.name1=
+device.name2=
+device.name3=
 device.name4=
 device.name5=
 
@@ -213,16 +213,6 @@ dump_boot;
 
 # begin ramdisk changes
 
-# init.rc
-backup_file init.rc;
-replace_string init.rc "cpuctl cpu,timer_slack" "mount cgroup none /dev/cpuctl cpu" "mount cgroup none /dev/cpuctl cpu,timer_slack";
-append_file init.rc "run-parts" init;
-
-# init.tuna.rc
-backup_file init.tuna.rc;
-insert_line init.tuna.rc "nodiratime barrier=0" after "mount_all /fstab.tuna" "\tmount ext4 /dev/block/platform/omap/omap_hsmmc.0/by-name/userdata /data remount nosuid nodev noatime nodiratime barrier=0";
-append_file init.tuna.rc "dvbootscript" init.tuna;
-
 # init.superuser.rc
 if [ -f init.superuser.rc ]; then
   backup_file init.superuser.rc;
@@ -232,13 +222,6 @@ else
   replace_file init.superuser.rc 750 init.superuser.rc;
   insert_line init.rc "init.superuser.rc" after "on post-fs-data" "    import /init.superuser.rc";
 fi;
-
-# fstab.tuna
-backup_file fstab.tuna;
-patch_fstab fstab.tuna /system ext4 options "nodiratime,barrier=0" "nodev,noatime,nodiratime,barrier=0,data=writeback,noauto_da_alloc,discard";
-patch_fstab fstab.tuna /cache ext4 options "barrier=0,nomblk_io_submit" "nosuid,nodev,noatime,nodiratime,errors=panic,barrier=0,nomblk_io_submit,data=writeback,noauto_da_alloc";
-patch_fstab fstab.tuna /data ext4 options "nomblk_io_submit,data=writeback" "nosuid,nodev,noatime,errors=panic,nomblk_io_submit,data=writeback,noauto_da_alloc";
-append_file fstab.tuna "usbdisk" fstab;
 
 # end ramdisk changes
 
